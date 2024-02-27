37ede7d320491932cf2222031c7ec95c68d52599
BaiLiping <blp_engineer@outlook.com>
2024-02-27 16:14:32 +0100

    update

--- main.m
+++ main.m
@@ -6,53 +6,25 @@
 % in this project we used Adaptive line Enhancer(ALE) to predict the noise
 % i.e. the desired signal is set to be a delayed version of the measured data.
 
-% This file is standalone file. Can be run without any supporting function.
-
-% Methods with System Identification
-
-% The Steepest-Descent Algorithm derived with Expected MSE
-% MSE(n,theta) = rxx(n,n) − 2theta^{T}ΣYx(n) + theta^{T}ΣYY(n)theta
-% theta_hat(n) = theta_hat(n−1) − μ/2*∂MSE(n,theta)/∂theta∣theta=theta_hat(n−1)
-% theta_hat(n) = theta_hat(n − 1) − μ{ΣYY(n)theta_hat(n − 1) − ΣYx(n)}
-% has a computational cost of O(N^2)
-
-% Methods without System Identification
-
-% The Least Mean Square (LMS) Algorithm derived with instantaneous error
-% MSE_hat(n, theta) = {x(n) − theta^{T}Y(n)}^2
-% theta_hat(n) = theta_hat(n−1)−μ/2*∂MSE_hat(n,theta)/∂theta∣theta=theta_hat(n−1)
-% theta_hat(n) = theta_hat(n − 1) + μY(n)*{x(n) − x_hat(n)}
-% x_hat(n)=Y^{T}(n)theta_hat(n-1)
-% has a computational cost of O(N)
-
-% The Normalized LMS (NLMS) Algorithm has normalized step size
-% μ(n) = μ_bar/{c + ‖Y(n)‖_L2Norm}
-
-% The Recursive Least Square (RLS) Algorithm
-% Σ_hat_RLS_YY(n) = Y(n)Y^T(n) + λΣ_hat_RLS_YY(n − 1)
-% Σ_hat_RLS_Yx(n) = Y(n)x(n) + λΣ_hat_RLS_Yx(n − 1)
-% thetahat_RL(n) = ˆθ(n − 1) +μInv{Σ_hat_RLS_YY(n)}
-%                       {Σ_hat_RLS_Yx(n)−Σ_hat_RLS_YY(n)θ_hat(n − 1)}
-% has a computational cost of O(N^3)
-
+% This file is standalone file. Can be excuted without any supporting function.
 
 clc;
 clear all;
 clc;
 % Read data
 [z,fs]=audioread("EQ2401Project2data2024.wav");
-
+%[z,fs]=audioread("EQ2401Project2_bonus_task2024.wav");
 % set the filter parameters
 mu_lms = 0.004;
 mu_bar = 0.25;
-N = 3000;
+N = 300;
 M = length(z);
 D = 0;
 delay = dsp.Delay(D);
 x= delay(z);
 P = .05*eye(N);
 lambda = 0.9999999;
-c = 0;
+c = 1e-10;
 
 %Initiate data structure
 xhat_lms = zeros(M, 1);
@@ -75,25 +47,35 @@ var_y = zeros(1, M);
 for n = N:M
     % Y (n) = [y(n), y(n − 1), . . . , y(n − N + 1)]T 
     Y_n = z(n:-1:n-N+1);
+    % compute for the statistical characteristics of Y_n for plotting
     mean_y(n) = mean(Y_n);
     var_y(n) = var(Y_n);
-    %%LMS
+    
+    %%  Least Mean Square (LMS) Algorithm
+    % Instantaneous Error MSE(n, theta) = {x(n) − theta^{T}Y(n)}^2
+    % theta_hat(n) = theta_hat(n−1)−μ/2*∂MSE_hat(n,theta)/∂theta∣theta=theta_hat(n−1)
+    % theta_hat(n) = theta_hat(n − 1) + μY(n)*{x(n) − x_hat(n)}
+    % x_hat(n)=Y^{T}(n)theta_hat(n-1)
     xhat_lms(n) = Y_n' * thetahat_lms(:, n-1);  
     thetahat_lms(:, n) = thetahat_lms(:, n-1) + mu_lms * Y_n * (x(n) - xhat_lms(n));
 
-    %%NLMS
+    %% Normalized LMS (NLMS) Algorithm
     xhat_nlms(n) = Y_n' * thetahat_lms(:, n-1); 
-    % μ(n) = μ_bar/{c + ‖Y(n)‖_L2Norm}
+    % μ(n) = μ_bar/{c + ‖Y(n)‖L2Norm}
     mu_nlms(n) = mu_bar/(c+Y_n'*Y_n);
     thetahat_nlms(:,n) = thetahat_nlms(:,n-1) + mu_nlms(n) *Y_n*(x(n)- xhat_nlms(n)); 
 
-    %%RLS
-    xhat_rls(n)=xhat_nlms(n);
-    thetahat_rls(:,n)=thetahat_nlms(:,n);
-    %xhat_rls(n) = Y_n' * thetahat_rls(:,n-1);
-    %K=P*Y_n/(lambda+Y_n'*P*Y_n);
-    %P=1/lambda*(P-K*Y_n'*P);
-    %thetahat_rls(:,n)=thetahat_rls(:,n-1)+K*(x(n)-xhat_rls(n));
+    %% Recursive Least Square (RLS) Algorithm
+   
+    % Σ_hat_YY(n) = Y(n)Y^T(n) + λΣ_hat_YY(n − 1)
+    % Σ_hat_Yx(n) = Y(n)x(n) + λΣ_hat_Yx(n − 1)
+    % θ_hat(n) = ˆθ(n − 1) +μInv{Σ_hat_YY(n)}
+    %                       {Σ_hat_Yx(n)−Σ_hat_YY(n)θ_hat(n − 1)}
+  
+    xhat_rls(n) = Y_n' * thetahat_rls(:,n-1);
+    K=P*Y_n/(lambda+Y_n'*P*Y_n);
+    P=1/lambda*(P-K*Y_n'*P);
+    thetahat_rls(:,n)=thetahat_rls(:,n-1)+K*(x(n)-xhat_rls(n));
 end
 
 % Store data
@@ -140,20 +122,19 @@ grid on;
 figure;
 plot(mu_nlms);
 xlabel('Time (Sample)');
-ylabel('μ(n) = μ_bar/{c + ‖Y(n)‖_L2Norm}')
+ylabel('μ(n) = μ_bar/{c + ‖Y(n)‖L2Norm}')
 grid on;
 
 
 figure;
-subplot(2,1,1); % Plot mean on the first subplot
+subplot(2,1,1); % Plot mean of Y_n
 plot(N:M, mean_y(N:M));
 title('Mean of y_n Over Time');
 xlabel('Time (Sample)');
 ylabel('Mean');
 
-subplot(2,1,2); % Plot variance on the second subplot
+subplot(2,1,2); % Plot variance of Y_n
 plot(N:M, var_y(N:M));
 title('Variance of y_n Over Time');
 xlabel('Time (Sample)');
 ylabel('Variance');
-
